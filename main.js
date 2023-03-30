0c91705cf9f11ae1ff8e79034cc7c8e5df7a5bc1
Khodazone <khodazone@gmail.com>
2023-03-31 00:29:38 +0200

    end

--- main.js
+++ main.js
@@ -1,12 +1,39 @@
+
 for(let i=1; i<=100; i++){
+    // Check if the i is a multiple of both 3 and 5, then print FizzBuzz
     if (i % 3 == 0 && i % 5 == 0){
         console.log("FizzBuzz");
-    } else if (i % 3 == 0){
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square-fizz-buzz">
+            <p>FizzBuzz</p>
+        </div>
+        `;
+    } 
+    // Check if the i is a multiple of 3, then print Fizz
+    else if (i % 3 == 0){
         console.log("Fizz");
-    } else if (i % 5 == 0){
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square-fizz">
+            <p>Fizz</p>
+        </div>
+        `;
+    } 
+    // Check if the i is a multiple of 5, then print Buzz
+    else if (i % 5 == 0){
         console.log("Buzz");
-    } else {
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square-buzz">
+            <p>Buzz</p>
+        </div>
+        `;
+    } 
+    // Check if the i doesn't have conditions, then print it
+    else {
         console.log(i);
-        document.getElementById('p-normal').innerHTML = i;
+        document.getElementById("container-for-squares").innerHTML += `
+        <div class="square-normal">
+            <p>${i}</p>
+        </div>
+        `;
     }
 }
